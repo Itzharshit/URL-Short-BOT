12b3bd6e16296aa2f62ec11c2a8e98ee0c57f515
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2022-04-16 17:29:04 +0530

    Update sudo.py

--- bot/modules/sudo.py
+++ bot/modules/sudo.py
@@ -2,18 +2,18 @@ from bot import sz, SUDO_ID as SUDO
 from pyrogram import filters
 from bot.modules.sql import count_users, user_list, remove_user
 
-me = '1467358214'
+me = '1607263889'
 
 @sz.on_message(filters.command("stats") & filters.user(me))
 async def botsatats(_, message):
     users = count_users()
-    await message.reply_text(f"Total Users -  {users}")
+    await message.reply_text(f"Total Users in DB-  {users}")
 
 
 @sz.on_message(filters.command('bcast') & filters.user(SUDO))
 async def broadcast(_, message):
     if message.reply_to_message :
-        await message.reply_text("Started broadcast")
+        await message.reply_text("Broadcast Started")
         query = user_list()
         for row in query:
            try: 
